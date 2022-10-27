6d72a136098546cd50b5f2de19662ac09b5b67b7
dsalle03 <dsalle2003@gmail.com>
2022-10-27 20:55:40 +0200

    Commit1

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,3 +1,17 @@
 import hashlib
 
+def tabla_1(tamanio):
+    tabla = [None] * tamanio
+    return tabla
 
+def numero_elementos(tabla):
+    return len(tabla) - tabla.count(None)
+
+def hash(dato, tamanio_tabla):
+    return len(str(dato).strip())% tamanio_tabla
+
+def anadir(tabla, dato):
+    posicion = hash(dato, len(tabla))
+    if (tabla[posicion] is None):
+        tabla[posicion] = lista()
+    insertar(tabla[posicion],dato)
---
+++ Ejercicio7.py
@@ -0,0 +1,10 @@
+class polinomio():
+    def __init__(self):
+        self.grado = -1
+        self.cabeza = None
+    def __str__(self):
+        nodo = self.cabeza
+    
+        str_polinomio = "{}x{}"
+
+    
